e219aaa5bb44791016b5aabdb82961af25e44cf6
VictoSN <victorionanda1105@gmail.com>
2026-05-01 06:22:51 +0800

    added try/catch to server routes

--- src/server/routes/notes.js
+++ src/server/routes/notes.js
@@ -4,28 +4,48 @@ import Note from "../models/Note.js"
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-    const notes = await Note.find();
-    res.json(notes);
+    try {
+        const notes = await Note.find();
+        res.json(notes);
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 router.get('/:id', async(req, res) => {
-    const notes = await Note.findById(req.params.id);
-    res.json(notes);
+    try {
+        const notes = await Note.findById(req.params.id);
+        res.json(notes);
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 router.post('/', async(req, res) => {
-    const notes = await Note.create(req.body);
-    res.json(notes);
+    try {
+        const notes = await Note.create(req.body);
+        res.json(notes);
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 router.delete('/:id', async(req, res) => {
-    await Note.findByIdAndDelete(req.params.id);
-    res.json({ success: true });
+    try {
+        await Note.findByIdAndDelete(req.params.id);
+        res.json({ success: true });
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 router.patch('/:id', async(req, res) => {
-    const update = await Note.findByIdAndUpdate(req.params.id, req.body, { returnDocument: "after" });
-    res.json(update);
+    try {
+        const update = await Note.findByIdAndUpdate(req.params.id, req.body, { returnDocument: "after" });
+        res.json(update);
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 export default router;
